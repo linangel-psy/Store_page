2ff6df6bfebcab9884ea200cc490b2ed34bd4891
linangel.psy <linangel.psy@gmail.com>
2015-12-29 20:04:23 +0000

    jade+html

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,130 @@
-var gulp = require('gulp');
-var jade = require('gulp-jade');
+'use strict';
 
-var plumber = require('gulp-plumber');
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    browserSync = require('browser-sync'),
+    uglify = require('gulp-uglify'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    autoprefixer = require('gulp-autoprefixer'),
+    jade = require('gulp-jade'),
+    spritesmith = require('gulp.spritesmith'),
+    plumber = require('gulp-plumber');
 
-var jadePath = 'src/jade/*.jade';
 
-gulp.task('jade', function() {
-	var YOUR_LOCALS = {};
+/* --------- paths --------- */
+var paths = {
+    sass: {
+    	src: [
+            'src/sass/**/*.sass'
+        ],
+        location: [
+            'bower_components/normalize-sass/_normalize.sass',
+            'src/sass/main.sass'
+        ],
+        destination: 'www/css'
+    },
 
-	gulp.src(jadePath)
-		.pipe(plumber())
-		.pipe(jade({
-			locals: YOUR_LOCALS,
-			pretty: '\t'
-		}))
-		.pipe(gulp.dest('www'))
+    js: {
+    	src: [
+    		'src/js/**/*.js'
+    	],
+        location: [
+            'bower_components/jquery/dist/jquery.js',
+            'src/js/main.js'
+        ],
+        destination: 'www/js'
+    },
+
+    jade: {
+    	src: [
+    		'src/jade/**/*.jade'
+    	],
+        location: [
+            'src/jade/*.jade'
+        ],
+	    destination: 'www'
+	}
+};
+
+
+/* ----- jade ----- */
+gulp.task('jade-compile', function () {
+    gulp.src(paths.jade.location)
+        .pipe(plumber())
+        .pipe(jade({
+            pretty: '\t'
+        }))
+        .pipe(gulp.dest(paths.jade.destination))
 });
 
-gulp.task('watch', function(){
-	gulp.watch(jadePath,['jade'])
+
+/* ------ sass ------ */
+gulp.task('sass-compile', function () {
+    gulp.src(paths.sass.location)
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(sass.sync({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(autoprefixer(['> 5%', 'last 5 versions', 'IE 9']))
+        .pipe(concat("main.min.css"))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(paths.sass.destination));
 });
+
+
+/* -------- concat js -------- */
+gulp.task('concat-js', function () {
+    return gulp.src(paths.js.location)
+        .pipe(sourcemaps.init())
+        .pipe(concat('main.min.js'))
+        .pipe(uglify())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(paths.js.destination));
+});
+
+
+/* -------- auto sprites  -------- */
+gulp.task('sprite', function () {
+    var spriteData = gulp.src('src/img/*.png')
+        .pipe(spritesmith({
+            imgName: 'sprite.png',
+            imgPath: '../img/sprite.png',
+            cssName: 'sprite.sass'
+        }));
+    spriteData.img.pipe(gulp.dest('www/img/'));
+    spriteData.css.pipe(gulp.dest('src/sass/'));
+});
+
+
+/* -------- gulp server  -------- */
+gulp.task('server', function () {
+    browserSync({
+        port: 9000,
+        browser: ['google chrome'],
+        server: {
+            baseDir: 'www'
+        }
+    });
+});
+
+
+/* -------- gulp watching  -------- */
+gulp.task('watch', function () {
+    gulp.watch(paths.jade.src, ['jade-compile']);
+    gulp.watch(paths.sass.src, ['sass-compile']);
+    gulp.watch(paths.js.src, ['concat-js']);
+    gulp.watch([
+        'www/*.html',
+        'www/css/*css',
+        paths.js.src
+    ]).on('change', browserSync.reload);
+});
+
+
+gulp.task('default', [
+    'jade-compile',
+    'sass-compile',
+    'concat-js',
+    'server',
+    'watch'
+]);
